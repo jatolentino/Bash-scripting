ee242584b42fbad613d77367c6e3f56f7f924a05
josetv91 <61167951+josetv91@users.noreply.github.com>
2021-02-06 18:03:34 -0500

    Update sercom.sh

--- sercom.sh
+++ sercom.sh
@@ -4,3 +4,12 @@ touch /tmp/myCommand.txt
 vare=/tmp/myCommand.txt
 # $! = command $2 string
 apropos $1 | awk '{print $1}' > $vare
+#vare=my.txt
+sed -e 's/^/man /' -i $vare
+#vare=my.txt
+while read line
+do
+  #$line | grep -wn $2 | grep -wn $3 && echo "$(tput setaf 4)$(tput setab 6)$line$(tput sgr0)"
+  #$line | grep -A 2 -B 2 -wn $2 && echo "$(tput setaf 4)$(tput setab 6)$line$(tput sgr0)"
+  #$line | grep -A 2 -B 2 '$2' && grep :'$3.\*$2' && echo "$(tput setaf 4)$(tput setab 6)$line$(tput sgr0)"
+  
