9b61f5adc6776529af9577dec6eff3dbb6b671a6
josetv91 <61167951+josetv91@users.noreply.github.com>
2021-02-06 18:16:02 -0500

    Update sercom.sh
    Search for commands related to a command within MAN pages based on two wildcard strings ordered by priority

--- sercom.sh
+++ sercom.sh
@@ -2,7 +2,7 @@
 #read commandToFind stringToFind
 touch /tmp/myCommand.txt
 vare=/tmp/myCommand.txt
-# $! = command $2 string
+# $1 command $2 string $3 2nd priority string
 apropos $1 | awk '{print $1}' > $vare
 #vare=my.txt
 sed -e 's/^/man /' -i $vare
@@ -11,4 +11,9 @@ while read line
 do
   $line | grep -A 2 -B 2 -wn $2 && grep -wn $3 | echo "$(tput setaf 4)$(tput setab 6)$line$(tput sgr0)"
 done < $vare
-  
+
+# Give executable permissions 
+# $ sudo chmod u+x sercom.sh 
+# Run as:
+# $ ./sercom.sh command 1ststring 2ndstring
+# e.g: ./sercom.sh selinux port tcp
