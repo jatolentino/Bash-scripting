ff52ad9d2c7be195a34c11633165e8c490882f92
josetv91 <61167951+josetv91@users.noreply.github.com>
2021-02-06 18:03:43 -0500

    Update sercom.sh

--- sercom.sh
+++ sercom.sh
@@ -12,4 +12,18 @@ do
   #$line | grep -wn $2 | grep -wn $3 && echo "$(tput setaf 4)$(tput setab 6)$line$(tput sgr0)"
   #$line | grep -A 2 -B 2 -wn $2 && echo "$(tput setaf 4)$(tput setab 6)$line$(tput sgr0)"
   #$line | grep -A 2 -B 2 '$2' && grep :'$3.\*$2' && echo "$(tput setaf 4)$(tput setab 6)$line$(tput sgr0)"
+  #if [-z `($line | grep -wn $2)` ] then
+  #       echo "yes"
+  #else
+  #       echo "no"
+  #fi
+  $line | grep -A 2 -B 2 -wn $2 && grep -wn $3 | echo "$(tput setaf 4)$(tput setab 6)$line$(tput sgr0)"
+  #$line | grep -wne $2 -e $3 && echo "$(tput setaf 4)$(tput setab 6)$line$(tput sgr0)"
+#$sgr0
+#line | grep -wn $2 && echo "$line"
+#$line | grep -l port
+done < $vare
+#sed -i 's/man //' -i $vare
+
+
   
