70254465bb99f561b5a00053ebe4e366a0e08d33
josetv91 <61167951+josetv91@users.noreply.github.com>
2021-02-07 01:28:42 -0500

    Update sercom.sh

--- sercom.sh
+++ sercom.sh
@@ -29,5 +29,4 @@ done < $vare
   #else
   #       echo "no"
   #fi
-  #$line | grep -A 2 -B 2 -wn $2 && grep -wn $3 | echo "$(tput setaf 4)$(tput setab 6)$line$(tput sgr0)"
-  #$line | grep -wne $2 -e $3 && echo "$(tput setaf 4)$(tput setab 6)$line$(tput sgr0)"
+ 
