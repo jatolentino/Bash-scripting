303fa39d451c60c6100551cbe8a294ecf2b2a29b
josetv91 <61167951+josetv91@users.noreply.github.com>
2021-03-01 22:57:48 -0500

    Update scriptrepo.sh

--- scriptrepo.sh
+++ scriptrepo.sh
@@ -1,9 +1,10 @@
 #!/usr/bin
 mkdir /rpms
 cp -r /mnt/BaseOS.repo /rpms/ 
-echo "[BaseOS]
+echo "
+[BaseOS]
 name= BaseOS repo from my disk
 baseurl= file:///rpm/BaseOS
 enabled = 1
 gpgcheck = 0
-#gpgkey = file:///etc/pki/rpm-gpg/RPM-GPG-KEY-release" > /etc/yum.repos.d/BaseOS.repo
+gpgkey = file:///etc/pki/rpm-gpg/RPM-GPG-KEY-release" > /etc/yum.repos.d/BaseOS.repo
