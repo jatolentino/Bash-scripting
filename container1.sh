58f19a8a4a3361b33eac8901ca4a7bb4d6f2981d
josetv91 <61167951+josetv91@users.noreply.github.com>
2021-03-21 23:43:12 -0500

    Update container1.sh

--- container1.sh
+++ container1.sh
@@ -17,4 +17,13 @@ cd journal
 ls
 exit
 
+mkdir ~/containter-logserver
+cp /var/log/ ~/contianer-logserver
+podman run -d --name logserver /home/wallah/container-logserver/:/var/log/:Z rsyslog
+podman ps
 
+mkdir ~/.config/systemd/user
+cd ~/.config/systemd/user
+podman generate systemd logserver > container-logserver.service
+vim container-logserver.service
+WantedBy=default.target
