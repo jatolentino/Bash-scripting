9e773816b4b163b882f9a69f4d638af27d23e93c
josetv91 <61167951+josetv91@users.noreply.github.com>
2021-03-21 23:37:21 -0500

    Update container1.sh

--- container1.sh
+++ container1.sh
@@ -1,1 +1,11 @@
+#!/usr/bin
+useradd wallah
+passwd wallah
+yum install - container-tools -y
+exit
+
+ssh wallah@192.168.1.101
+podman login redhat.io.registry
+# include user and pass
+podman pull redhat.io.registry/rsyslog
 
