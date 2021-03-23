c8ff5ac6ce5e58f25897bc22c4212dcf55cc4206
josetv91 <61167951+josetv91@users.noreply.github.com>
2021-03-23 00:43:59 -0500

    Update connectionconfig.sh

--- connectionconfig.sh
+++ connectionconfig.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 nmcli c m `nmcli -g name c s` ipv4.address 192.168.1.101/24 ipv4.gateway 192.168.1.1 ipv4.dns 192.168.1.1 ipv4.method manual connection.autoconnect yes
 nmcli c reload
+ifconfig
 hostnamectl set-hostname node1-rhel.example.com
 
