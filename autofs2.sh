f7939741a511b4fbdef32fdd6e747e73842541b2
josetv91 <61167951+josetv91@users.noreply.github.com>
2021-03-26 01:35:04 -0500

    Create autofs2.sh
    PC2 shares /remoteuser/Dirshare

---
+++ autofs2.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+# input the other pc IP, directory shared in autofs1.sh is /Dirshare
+yum install autofs* nfs-utils*
+showmount -e 192.168.1.102
+mkdir -p /remoteuser/Dirshare
+echo "/- /etc/auto.remote" >> /etc/auto.master.d/remote.autofs
+echo "/remoteuser/Dirshare -rw,sync, 192.168.1.102:/Dirshare" >> /etc/auto.remote
+systemctl enable --now autofs
