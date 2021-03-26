8e2d1a47bc6e8aecc3f20ae3f004fe377cd243f9
josetv91 <61167951+josetv91@users.noreply.github.com>
2021-03-26 01:35:57 -0500

    Update autofs1.sh

--- autofs1.sh
+++ autofs1.sh
@@ -5,6 +5,6 @@ firewall-cmd --permanent --add-service={nfs,rpc-bind,mountd}
 firewall-cmd --reload
 mkdir /Dirshare
 # add the other computer's ip
-echo "/- 192.168.1.1(rw,sync,no_root_squash)" > /etc/exports
+echo "/Dirshare 192.168.1.1(rw,sync,no_root_squash)" > /etc/exports
 systemctl restart nfs-server
 
