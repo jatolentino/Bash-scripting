fe388cf5ce24c001711362d60357345766557338
josetv91 <61167951+josetv91@users.noreply.github.com>
2021-03-26 01:37:29 -0500

    Update autofs1.sh

--- autofs1.sh
+++ autofs1.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
+# PC IP 192.168.1.101
 yum install -y autofs* nfs-utils*
 systemctl restart nfs-server
 firewall-cmd --permanent --add-service={nfs,rpc-bind,mountd}
 firewall-cmd --reload
 mkdir /Dirshare
 # add the other computer's ip
-echo "/Dirshare 192.168.1.1(rw,sync,no_root_squash)" > /etc/exports
+echo "/Dirshare 192.168.1.101(rw,sync,no_root_squash)" > /etc/exports
 systemctl restart nfs-server
 
