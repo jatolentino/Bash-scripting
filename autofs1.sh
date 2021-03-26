fd373f3085cacb6ebc861e2e5e2900bcb7149806
josetv91 <61167951+josetv91@users.noreply.github.com>
2021-03-26 01:24:06 -0500

    Update autofs1.sh

--- autofs1.sh
+++ autofs1.sh
@@ -1,1 +1,9 @@
-yum install -y 
+#!/bin/bash
+yum install -y autofs* nfs-utils*
+systemctl restart nfs-server
+firewall-cmd --permanent --add-service={nfs,rpc-bind,mountd}
+firewall-cmd --reload
+mkdir /Dirshare
+# add the other computer's ip
+echo "/- 192.168.1.1(rw,sync,no_root_squash)" > /etc/exports
+systemctl restart nfs-server
