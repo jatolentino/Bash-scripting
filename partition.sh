65d58dbeded74d3f962bd5b1b9f5d4f5ef24272d
josetv91 <61167951+josetv91@users.noreply.github.com>
2021-04-07 23:32:07 -0500

    Update partition.sh

--- partition.sh
+++ partition.sh
@@ -7,3 +7,5 @@ read sdVar
 echo "Please print the number of the partition, 1-4"
 read sdNum
 echo "Please print the size you want your partition to be, ex 200M, 5G (no more than 15G)"
+read sdSize
+(echo o; echo n; echo p; echo $sdNum; echo; echo +$sdSize; echo t; echo $sdNum; echo c; echo w) | fdisk /dev/$sdVar
